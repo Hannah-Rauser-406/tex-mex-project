6c62169881eba0239a61aafe310aac5bac970c73
Hannah Hendrickson <hannahhendrickson@Hannahs-MBP.PK5001Z>
2017-08-02 09:07:00 -0600

    still an error message in the spicy filter section lines 10 and 13

--- src/Spicy/SpiceLevel.js
+++ src/Spicy/SpiceLevel.js
@@ -4,24 +4,18 @@ import data from '../data';
 
 
 const SpiceLevel = (props) => {
-  console.log('spicy')
- const newSpice = props.items
- .filter(function(item){
-   return item >= 5;
-   console.log('spicy filter')
- }).map(function(item, index){
-   return <li key={index} > {item} </li>
-   console.log('spicy map')
+  console.log('spicy', data)
+ const newSpice = data.items.filter(function(newSpice, index){
+   return newSpice >= 5;
+   console.log('spicy filter', data)
+ }).map(function(newSpice, index){
+   return <li key={index}> {newSpice} </li>
+   console.log('spicy map', data)
  })
   return(
     <div>
-    <h1>Spicy items</h1>
-      <ul>
-        {SpiceLevel}
-      </ul>
-      <ul>
-        {newSpice}
-      </ul>
+    <h1>Spicy items over 5</h1>
+      <h3>{newSpice}</h3>
     </div>
   )
 }
